50727ca3380c64dc8757b3a1381c96e20e78d201
Yingzi1982 <yingzi.ying@me.com>
2023-03-17 11:09:57 +0100

    pushing to Github

--- fortran/modified/piezo/calculate_piezo_stress.f90
+++ fortran/modified/piezo/calculate_piezo_stress.f90
@@ -8,47 +8,50 @@
 !
 !========================================================================
 
-subroutine calculate_piezo_stress(receiver_position, piezo_stress)
+subroutine calculate_piezo_stress(dummy_coord_x,dummy_coord_z,dummy_sigma_xx,dummy_sigma_zz,dummy_sigma_xz,dummy_sigma_zx)
 
 use piezo_par, only : vacuum_permittivity, piezoelectric_constant_2d, charges, charge_number 
 
 use constants, only : PI
+
 use constants, only : myrank, IMAIN, CUSTOM_REAL
 
 
 implicit none
 
-real(kind=CUSTOM_REAL), dimension(2), intent(in) :: receiver_position
-real(kind=CUSTOM_REAL), dimension(3), intent(out) :: piezo_stress 
+
+real(kind=CUSTOM_REAL), intent(in) :: dummy_coord_x, dummy_coord_z
+
+real(kind=CUSTOM_REAL), intent(out) :: dummy_sigma_xx, dummy_sigma_zz, dummy_sigma_xz, dummy_sigma_zx
 
 real(kind=CUSTOM_REAL) :: charge_value
+
 real(kind=CUSTOM_REAL), dimension(2) :: charge_position
 
-real(kind=CUSTOM_REAL) :: potential
 real(kind=CUSTOM_REAL), dimension(2) :: electric
+real(kind=CUSTOM_REAL), dimension(2) :: total_electric
 
-real(kind=CUSTOM_REAL), dimension(2) :: diff
+real(kind=CUSTOM_REAL), dimension(2) :: displacement
+
+real(kind=CUSTOM_REAL), dimension(3) :: dummy_sigma
+real(kind=CUSTOM_REAL), dimension(2) :: dummy_coord
 
 integer :: i_charge
 
-!integer :: ii,jj
-
-electric(:) = 0.0
-
+total_electric(:) = 0._CUSTOM_REAL
 do i_charge = 1,charge_number
   charge_position = charges(i_charge,1:2)
   charge_value = charges(i_charge,3)
-  diff = receiver_position - charge_position
-  write(IMAIN,*) 'diff', diff(1), diff(2)
-  !potential = 
-  write(IMAIN,*) 'electric', electric(1), electric(2)
-  electric = electric + charge_value/(4.0*PI*vacuum_permittivity)/((norm2(diff))**3.0)*diff
+  dummy_coord = [dummy_coord_x,dummy_coord_z]
+  displacement = dummy_coord - charge_position
+  electric = -charge_value*displacement/(norm2(displacement)**2)
+  total_electric = total_electric + electric
 end do
-  !write(IMAIN,*) 'electric', electric(1), electric(2), norm2(electric)
 
-piezo_stress = -matmul(transpose(piezoelectric_constant_2d),electric)
-!write(IMAIN,*) 'print piezo stress', piezo_stress(1), piezo_stress(2), piezo_stress(3)
-!'(e16.6)'
-!'(e10.3)'
-!'(e10.3)'
+dummy_sigma = -matmul(transpose(piezoelectric_constant_2d),electric)
+
+dummy_sigma_xx = dummy_sigma(1)
+dummy_sigma_zz = dummy_sigma(2)
+dummy_sigma_xz = dummy_sigma(3)
+dummy_sigma_zx = dummy_sigma_xz
 end subroutine
